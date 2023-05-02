19b5a019fbf86af7b09a5380f83176538ac287d9
edsonwade <vanilsonmuhongo@gamil.com>
2023-05-02 11:26:39 +0100

    Environment building

---
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping(path = "/")
+public class HomeController {
+
+    @Value("${app.version}")
+    private String appVersion;
+
+    @GetMapping
+    public Map<String, String> getStatus() {
+        Map<String, String> map = new HashMap<>();
+        map.put("app-version", appVersion);
+        return map;
+    }
+
+}
